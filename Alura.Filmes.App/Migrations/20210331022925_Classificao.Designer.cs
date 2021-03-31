9d21c0022c2d2f34b0382c78e079be0b1866b8d6
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2021-03-30 23:46:39 -0300

    Adding column at films' table to receive rating

--- Alura.Filmes.App/Migrations/20210331022925_Classificao.Designer.cs
+++ Alura.Filmes.App/Migrations/20210331022925_Classificao.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Alura.Filmes.App.Migrations
 {
     [DbContext(typeof(AluraFilmesContext))]
-    partial class AluraFilmesContextModelSnapshot : ModelSnapshot
+    [Migration("20210331022925_Classificao")]
+    partial class Classificao
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
