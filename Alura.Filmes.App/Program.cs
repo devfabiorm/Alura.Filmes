3ee91664b3f028ba7b384cc1c923194a74563fc1
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2021-04-05 23:37:25 -0300

    Using Stored Procedure with EF Core

--- Alura.Filmes.App/Program.cs
+++ Alura.Filmes.App/Program.cs
@@ -3,6 +3,7 @@ using Alura.Filmes.App.Extensions;
 using Alura.Filmes.App.Negocio;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Data.SqlClient;
 using System.Linq;
 
 namespace Alura.Filmes.App
@@ -23,18 +24,19 @@ namespace Alura.Filmes.App
             using (var contexto = new AluraFilmesContext())
             {
                 contexto.LogSQLToConsole();
+                var categ = "Action";
+                var paramCateg = new SqlParameter("category_name", categ);
 
-                var sql = @"select a.*
-                from actor a
-                inner join
-                top5_most_starred_actors filmes on filmes.actor_id = a.actor_id";
-
-                var atoresMaisAtuantes = contexto.Atores.FromSql(sql).Include(a => a.Filmografia);
-
-                foreach (var ator in atoresMaisAtuantes)
+                var paramTotal = new SqlParameter
                 {
-                    Console.WriteLine($"O ator {ator.PrimeiroNome} {ator.UltimoNome} atuou em {ator.Filmografia.Count} filmes");
-                }
+                    ParameterName = "@total_actors",
+                    Size = 4,
+                    Direction = System.Data.ParameterDirection.Output
+                };
+
+                contexto.Database.ExecuteSqlCommand("total_actors_from_given_category @category_name, @total_actors OUT", paramCateg, paramTotal);
+
+                Console.WriteLine($"O número total de atores que atuaram na categoria {categ} é de {paramTotal.Value} atores.");
             }
         }
     }
