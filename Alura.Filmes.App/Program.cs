3aa2201253d6dc61ea1906f70b2d7fe3ab57f186
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2021-04-05 23:50:08 -0300

    Insert and delete operation using ExecuteSqlCommand from EF Core

--- Alura.Filmes.App/Program.cs
+++ Alura.Filmes.App/Program.cs
@@ -12,7 +12,6 @@ namespace Alura.Filmes.App
     {
         static void Main(string[] args)
         {
-
             //var test = ClassificacaoIndicativa.MaioresQue10;
 
             //foreach(ClassificacaoIndicativa item in Enum.GetValues(typeof(ClassificacaoIndicativa)))
@@ -20,24 +19,38 @@ namespace Alura.Filmes.App
             //    Console.WriteLine(item.ParaString());
             //}
 
-
-            using (var contexto = new AluraFilmesContext())
+            using(var contexto = new AluraFilmesContext())
             {
                 contexto.LogSQLToConsole();
-                var categ = "Action";
-                var paramCateg = new SqlParameter("category_name", categ);
 
-                var paramTotal = new SqlParameter
-                {
-                    ParameterName = "@total_actors",
-                    Size = 4,
-                    Direction = System.Data.ParameterDirection.Output
-                };
+                var sql = "INSERT INTO language (name) VALUES ('Teste 1'), ('Teste 2'), ('Teste 3')";
+                var registros = contexto.Database.ExecuteSqlCommand(sql);
+                Console.WriteLine($"O total de registros afetados é {registros}.");
 
-                contexto.Database.ExecuteSqlCommand("total_actors_from_given_category @category_name, @total_actors OUT", paramCateg, paramTotal);
-
-                Console.WriteLine($"O número total de atores que atuaram na categoria {categ} é de {paramTotal.Value} atores.");
+                var deleteSql = "DELETE FROM language WHERE name LIKE 'Teste%'";
+                registros = contexto.Database.ExecuteSqlCommand(deleteSql);
+                Console.WriteLine($"O total de registros afetados é {registros}.");
             }
+
+        }
+
+        private static void StoredProcedure(DbContext contexto)
+        {
+
+            var categ = "Action";
+            var paramCateg = new SqlParameter("category_name", categ);
+
+            var paramTotal = new SqlParameter
+            {
+                ParameterName = "@total_actors",
+                Size = 4,
+                Direction = System.Data.ParameterDirection.Output
+            };
+
+            contexto.Database.ExecuteSqlCommand("total_actors_from_given_category @category_name, @total_actors OUT", paramCateg, paramTotal);
+
+            Console.WriteLine($"O número total de atores que atuaram na categoria {categ} é de {paramTotal.Value} atores.");
+
         }
     }
 }
